83372843ea20f44eb498b47d267e42e8a16126c9
valdezjulian <julian.valdez531@gmail.com>
2024-11-05 13:57:16 -0400

    alteração link deploy - projeto linktree

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -237,7 +237,7 @@ function projetos(section) {
             </svg>
           </a>
 
-          <a href="https://linketree-julian-valdez.vercel.app/" id="desafio" title="Ver Projetos">
+          <a href="https://linktree-julian-valdez.vercel.app/" id="desafio" title="Ver Projetos">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
               <path fill="none" d="M0 0h24v24H0z" />
               <path
